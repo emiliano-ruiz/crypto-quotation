7bbe31d9b017a4dcd1c679bd3c4fca7a1c202f78
Emiliano Ruiz Carletti <eruiz42100@gmail.com>
2020-10-01 22:22:47 -0300

    Updates

---
+++ precache-manifest.8595604f4f51f36b359346aa0d25b4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793ebbe0d6a26708d8085a3df92a37c3",
+    "url": "/crypto-quotation/index.html"
+  },
+  {
+    "revision": "96706ddcfd2223427fe9",
+    "url": "/crypto-quotation/static/css/main.8f7cc46b.chunk.css"
+  },
+  {
+    "revision": "ea2f3b0d78d7eba1530a",
+    "url": "/crypto-quotation/static/js/2.98fd701d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crypto-quotation/static/js/2.98fd701d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96706ddcfd2223427fe9",
+    "url": "/crypto-quotation/static/js/main.8936e40a.chunk.js"
+  },
+  {
+    "revision": "35878d6fc0fb076ee10a",
+    "url": "/crypto-quotation/static/js/runtime-main.73a15424.js"
+  },
+  {
+    "revision": "35f8cae90eda9e49134aad1e3c2e6c81",
+    "url": "/crypto-quotation/static/media/cryptoimg.35f8cae9.png"
+  }
+]);
